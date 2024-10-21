f20af891b5abe6241f62732f35d73d6a50df42e7
bemos <bemo.0@mail.ru>
2024-10-21 21:18:23 +0300

    новый экран

---
+++ Himical/DatabasePage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Himical
+{
+    /// <summary>
+    /// Логика взаимодействия для DatabasePage.xaml
+    /// </summary>
+    public partial class DatabasePage : Page
+    {
+        public DatabasePage()
+        {
+            InitializeComponent();
+            ProductsGrid.ItemsSource = ProductsDbEntities.GetContext().Products.ToList();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
